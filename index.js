ce5fdd18a3062275efb81075059f9a37379a4fec
OPM130 <operez2@misena.edu.co>
2024-07-01 22:23:25 -0500

    prueba 19

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import routes from './config/routes.js'
 
-const app = express()
+/* const app = express()
 const corsOption={
     origin:'*'//'localhost:3000'
 }
@@ -22,4 +22,30 @@ const server=app.listen(process.env.PORT || 8080,()=>{
     console.log(`Servidor corriendo en puerto: ${server.address().port}`)
 })
 
-export default app
+export default app */
+
+const app = express();
+const corsOption = {
+    origin: '*' //'localhost:3000'
+};
+
+// Configuración del middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors(corsOption));
+app.use('/api', routes);
+
+app.get('/', (req, res) => {
+    console.log("Holas");
+    res.send('Bienvenidos a mi API');
+});
+
+// Verifica que la variable de entorno PORT esté configurada
+const PORT = process.env.PORT || 8080;
+console.log(`PORT: ${PORT}`);
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto: ${server.address().port}`);
+});
+
+export default app;
